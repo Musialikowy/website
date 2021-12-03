d01a389538d722fd82442f8583ff93890240ef6e
Musialikowy <m.musialik001@gmail.com>
2021-12-03 19:28:18 +0100

    Add files via upload

--- scripts/postmanager.js
+++ scripts/postmanager.js
@@ -1,6 +1,6 @@
 function renderpost(postid){
 	$.getJSON(postid, function(data){
-		alert(data.title + " " + data.text + " " + data.header);
+		alert(data.title + " " + data.header + " " + data.text);
 	}).fail(function(){
 		alert("An error has occurred.");
 	});
